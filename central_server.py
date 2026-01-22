ede3e548d224d36b2df6e4e56a8adf3e06790c72
Sabolf <Sabolf@gmail.com>
2026-01-22 14:16:12 +0100

    First commit

---
+++ central_server.py
@@ -0,0 +1,82 @@
+from typing import Union
+import httpx
+from fastapi import FastAPI
+import time
+app = FastAPI()
+
+# FAKE DATABASE OF ORDERS
+fakeDataBase = []
+id = 0
+#------------------LOCKER SERVERS
+LOCKER_SERVER = "http://127.0.0.1:8001"
+# --------------------------------------------------------------------------
+
+#---------------------ROOT
+@app.get("/")
+def read_root():
+    return {"CENTRAL": "SERVER"}
+#-------------------------------------------------------------------------
+
+
+# -------------------GET [ REQUEST ]  1
+# LOCKER STATUS BY IP 
+@app.get("/display-locker")
+async def check_locker_status():
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{LOCKER_SERVER}/status")
+        return {"locker_status": response.json()}
+    
+# ----------------------------------------------------------------------------
+
+#-------------------- POST [ RESPONSE ] 2
+# [ Locker Battery, TimeStamp]  --- can change later #####
+@app.post("/locker-update")
+async def receive_locker_update(data: dict):
+    print("SUCCESS: ", data, " received")
+    return({
+        "status" : data["battery"],
+        "time" : time.strftime("%H %M")
+        })
+# ------------------UPDATE REGISTRY AND HEALTH STATUS OF LOCKER 
+# UPDATE HAPPENS HERE
+
+# ---------------------------------------------------------------
+
+# PURCHASE PURCHASE PURCHASE PURCHASE PURCHASE PURCHASE PURCHASE PURCHASE PURCHASE PURCHASE
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# POST [ RESPONSE ] 3
+@app.post("/create-order")
+async def createOrder(orderInfo: dict):
+    print("CREATING ORDER...")
+    global id
+    
+    try:
+        id += 1
+        
+        orderID = orderInfo["orderID"]
+        productID = orderInfo["productID"]
+        quantity = orderInfo["quantity"]
+        lockerLoc = orderInfo["lockerLoc"]
+        
+        
+        tmpOrder = {
+            "id" : id,
+            "orderID" : orderID,
+            "productID" : productID,
+            "quantity" : quantity,
+            "lockerLoc" : lockerLoc,
+            "status" : "pending"
+        }
+        
+        fakeDataBase.append(tmpOrder)
+        return {"status" : "success"}
+        
+    except Exception as e:
+        print("SOMETHING WENT WRONG OOPSY")
+        return {"status" : str(e)}
+
+@app.get("/display-orders")
+async def displayOrders():
+    return {
+        "orders" : fakeDataBase
+    }
---
+++ locker_server.py
@@ -0,0 +1,74 @@
+import httpx
+import asyncio
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+# ----------------------------GENERAL INFO ABOUT LOCKER
+CENTRAL_SERVER_IP = "http://127.0.0.1:8000"
+
+locker_id_info = {
+    "name" : "Chicago01",
+    "location" : "ChicagoAddress",
+    "ip" : "static IP",
+}
+
+locker_status_info = {
+    "status" : "on",
+    "battery" : "77%",
+    "signal-strength" : "50dBm",
+    
+}
+
+# ---------------------------------------------------------------------------------
+
+#--------------------------------POST [ REQUEST ] 2
+# [UPDATING GENERAL INFO OF LOCKER TO CENTRAL SERVER]
+async def send_update():
+    while True:
+        try:
+    #on/off /temp/ humidity/ 
+            update_json = {
+                "status" : locker_status_info["status"],
+                "battery" : locker_status_info["battery"]
+            }
+    
+            async with httpx.AsyncClient() as client:
+                response = await client.post(
+                    f"{CENTRAL_SERVER_IP}/locker-update", json=update_json, timeout=10
+                )
+                
+                # return response.json()
+                print(response.json())
+        except Exception as e:
+            print("ISSUE")
+            
+        await asyncio.sleep(30)
+
+
+
+
+    # -------------------------------------LOOP TASK FOR SENDING UPDATES TO CENTRAL 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #Start UP background task
+    task = asyncio.create_task(send_update())
+    yield
+    #Shut DOWN
+    task.cancel()
+# ---------------------------------------------------------------------------------------------
+
+
+app = FastAPI(lifespan=lifespan)
+    
+
+# -------------------------ROOT
+@app.get("/")
+def read_root():
+    return {"LOCKER": "SERVER"}
+# ------------------------------------------------
+
+#---------------------------GET [ RESPONSE ]   1
+@app.get("/status")
+async def locker_status():
+    return {"TEST STATUS" : "OFF"}
+# -------------------------------------------------------------
